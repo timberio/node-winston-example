84ffa0b71b80a2a11c99ed37f26c2e93d0713289
Garet McKinley <self@mediachicken.com>
2017-08-29 16:28:02 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const timber = require('timber');
+const winston = require('winston');
+const express = require('express');
+
+// Initialize our express app
+const app = express();
+
+// Setup the default port to listen on
+const port = process.env.PORT || 8080
+
+// Initialize our timber transport stream
+const stream = new timber.transports.HTTPS('821_b4d0c518ed89279b:3790ad534227d2359029f9140f9dde9ed46e617850be4f77b782ffb79c4eba3a');
+
+// Add the timber tranport to winston and attach our stream
+winston.add(timber.transports.Winston, { stream });
+
+// Add the express middleware to log HTTP events
+app.use(timber.middlewares.express);
+
+
+// Configure timber...
+// The timber logger must be set so that the express middleware
+// uses the correct logger. The middleware logs to console by default
+timber.config.logger = winston;
+// Enable logging of the request/response body (defaults to false)
+timber.config.capture_request_body = true;
+timber.config.capture_response_body = true;
+
+
+// Create the index route
+app.get('/', (req, res) => {
+  res.send('Welcome to the index route :)');
+});
+
+// This route returns JSON data.
+// Since we enabled http body logging in the timber config,
+// the contents of the JSON object will be sent to timber.
+app.get('/json', (req, res) => {
+  res.json({
+    description: 'This is some sample JSON data',
+    date: new Date(),
+    get: req.params
+  });
+});
+
+// Start our express server
+app.listen(port, () => {
+  winston.log('info', `Server started on port ${port}`);
+});
